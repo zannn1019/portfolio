d96f8934a22365f27f18e58bff2003ef136d95c5
zannn1019 <fauzanguci1019@gmail.com>
2025-10-20 10:55:44 +0700

    change title

--- app/app.vue
+++ app/app.vue
@@ -7,7 +7,7 @@
 
 <script setup>
 useHead({
-  title: 'Portfolio - Creative Developer',
+  title: 'Ahmad Fauzan Portfolio',
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
