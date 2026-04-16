ce15a1ff13e0e7a88a070fedb3d7049e5561217c
zannn1019 <fauzanguci1019@gmail.com>
2026-04-16 14:14:45 +0700

    feat: add favicon assets and update nuxt configuration to include png icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,7 @@ export default defineNuxtConfig({
         { name: 'twitter:creator', content: '@zannn1019' },
       ],
       link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
